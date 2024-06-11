7eee989721c658b70bfd73c354d6020726bd6c3d
Rafael <rafael.sergio@nextpage.com.br>
2024-06-11 15:26:22 -0300

    feat: get organization members

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,46 @@
+import { roleSchema } from '@saas/auth/src/roles'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organization/:slug/membership',
+      {
+        schema: {
+          tags: ['organization'],
+          security: [{ bearerAuth: [] }],
+          summary: 'Get user membership on organization',
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string().uuid(),
+                role: roleSchema,
+                organizationId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            role: roleSchema.parse(membership.role),
+            id: membership.id,
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
