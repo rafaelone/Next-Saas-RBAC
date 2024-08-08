0048b249a039f3b19d2be96463b5c934411a2bac
Rafael <rafael.sergio@nextpage.com.br>
2024-08-07 23:37:52 -0300

    style: header

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -2,13 +2,17 @@ import Image from 'next/image'
 
 import nextIcon from '@/assets/nextIcon.svg'
 
+import { ProfileButton } from './profile-button'
+
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={nextIcon} className="size-6 dark:invert" alt="NextJs" />
       </div>
-      <div className="flex items-center gap-4"></div>
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
     </div>
   )
 }
